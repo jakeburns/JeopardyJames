3b233213df31197eff81048c6da7e961ce38016d
Jake Burns <jakeburns@mac.com>
2019-06-02 02:28:08 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+
+<h1>Hello World!</h1>
+
+</template>
+
+<script>
+import db from './db'
+export default{
+db.collection("James Info").get()
+  .then(querySnapshot => {
+    const items = []
+    querySnapshot.forEach((doc) => {
+    const item =  doc.data();
+    items.push(item);
+  });
+  console.log(items);
+  this.items = items;
+  });
+}
+
+</script>
+
+<style>
+@import url(https://bootswatch.com/4/darkly/bootstrap.min.css);
+</style>
